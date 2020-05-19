03269e23e5adeb20c8c47c7e25a07c90180de830
Felix Parker <flixpar@gmail.com>
2020-05-19 16:42:53 -0400

    improved patient allocation model

--- src/models/PatientAllocation.jl
+++ src/models/PatientAllocation.jl
@@ -14,7 +14,6 @@ function patient_allocation(
         admitted_patients::Array{Float32,2},
         discharged_patients::Array{Float32,2},
         adj_matrix::BitArray{2};
-        objective::Symbol=:overflow,
         hospitalized_days::Int=8,
         send_new_only::Bool=true,
         sendreceive_switch_time::Int=0,
@@ -31,7 +30,6 @@ function patient_allocation(
     @assert(size(beds, 1) == N)
     @assert(size(adj_matrix) == (N,N))
     @assert(size(discharged_patients) == (N, T))
-    @assert(objective == :overflow)
 
     model = Model(Gurobi.Optimizer)
     if !verbose set_silent(model) end
@@ -69,22 +67,6 @@ function patient_allocation(
         add_to_expression!(objective, setup_cost*sum(setup_dummy))
     end
 
-    # only send new patients if enabled
-    # otherwise only send less than active patients
-    if send_new_only
-        @constraint(model, [t=1:T],
-            sum(sent[:,:,t], dims=2) .<= admitted_patients[:,t]
-        )
-    else
-        @constraint(model, [t=1:T],
-            sum(sent[:,:,t], dims=2) .<=
-                initial_patients - sum(discharged_patients[:,1:min(t,hospitalized_days)], dims=2)
-                .+ sum(admitted_patients[:,max(1,t-hospitalized_days):t], dims=2)
-                .- sum(sent[:,:,1:t-1], dims=[2,3])
-                .+ sum(sent[:,:,max(1,t-hospitalized_days):t-1], dims=[1,3])
-        )
-    end
-
     # only send patients between connected locations
     for i = 1:N
         for j = 1:N
@@ -93,15 +75,6 @@ function patient_allocation(
             end
         end
     end
-
-    # ensure the number of active patients is non-negative
-    @constraint(model, [i=1:N,t=1:T],
-        0 <=
-            initial_patients[i] - sum(discharged_patients[i,1:min(t,hospitalized_days)])
-            + sum(admitted_patients[i,max(1,t-hospitalized_days):t])
-            - sum(sent[i,:,1:t])
-            + sum(sent[:,i,max(1,t-hospitalized_days):t])
-    )
 
     # enforce a minimum time between sending and receiving
     if sendreceive_switch_time > 0
@@ -113,33 +86,38 @@ function patient_allocation(
         )
     end
 
+    # expression for the number of active patients
+    @expression(model, active_patients[i=1:N,t=0:T]
+        initial_patients[i]
+        - sum(discharged_patients[i,1:t])
+        + sum(admitted_patients[i,max(1,t-hospitalized_days):t])
+        - sum(sent[i,:,1:t])
+        + sum(sent[:,i,max(1,t-hospitalized_days):t])
+    )
+
+    # expression for the patient overflow
+    @expression(model, overflow[i=1:N,t=1:T], active_patients[i,t] + sum(sent[i,:,t]) - beds[i])
+
+    # only send new patients if enabled
+    # otherwise only send less than active patients
+    if send_new_only
+        @constraint(model, [t=1:T], sum(sent[:,:,t], dims=2) .<= admitted_patients[:,t])
+    else
+        @constraint(model, [t=1:T], sum(sent[:,:,t], dims=2) .<= active_patients[:,t-1] .+ sum(sent[:,:,t], dims=1) - discharged_patients[:,t])
+    end
+
+    # ensure the number of active patients is non-negative
+    @constraint(model, [i=1:N,t=1:T], active_patients[i,t] >= 0)
+
     # load balancing
     if balancing_penalty > 0
         @variable(model, balancing_dummy[1:N,1:T] >= 0)
-        @constraint(model, [i=1:N,t=1:T],
-            balancing_dummy[i,t] >= ((
-                initial_patients[i] - sum(discharged_patients[i,1:min(t,hospitalized_days)])
-                + sum(admitted_patients[i,max(1,t-hospitalized_days):t])
-                - sum(sent[i,:,1:t])
-                + sum(sent[:,i,max(1,t-hospitalized_days):t])
-            ) / beds[i]) - balancing_thresh
-        )
+        @constraint(model, [i=1:N,t=1:T], balancing_dummy[i,t] >= (active_patients[i,t] / beds[i]) - balancing_thresh)
         add_to_expression!(objective, balancing_penalty * sum(balancing_dummy))
     end
 
-    # setup objective
-    flip_sign = (objective == :shortage) ? 1 : -1
-    z1, z2 = (objective == :shortage) ? (0, -1) : (-1, 0)
-    @constraint(model, [i=1:N,t=1:T],
-        obj_dummy[i,t] >= flip_sign * (
-            beds[i] - (
-                initial_patients[i] - sum(discharged_patients[i,1:min(t,hospitalized_days)])
-                + sum(admitted_patients[i,max(1,t-hospitalized_days):t])
-                - sum(sent[i,:,1:t+z1])
-                + sum(sent[:,i,max(1,t-hospitalized_days):t+z2])
-            )
-        )
-    )
+    # objective
+    @constraint(model, [i=1:N,t=1:T], obj_dummy[i,t] >= overflow[i,t])
 
     @objective(model, Min, objective)
 
