222cf2f99515d35c8a2098910f86fd95c5a681d2
Felix Parker <flixpar@gmail.com>
2020-06-07 22:07:47 -0400

    fixes for package

--- src/COVIDResourceAllocation.jl
+++ src/COVIDResourceAllocation.jl
@@ -4,12 +4,25 @@ include("models/PatientAllocation.jl")
 include("models/ReusableResourceAllocation.jl")
 include("models/DisposableResourceAllocation.jl")
 
-include("procecssing/BedsData.jl")
-include("procecssing/ForecastData.jl")
-include("procecssing/GeographicData.jl")
-include("procecssing/NurseData.jl")
+include("processing/BedsData.jl")
+include("processing/ForecastData.jl")
+include("processing/GeographicData.jl")
+include("processing/NurseData.jl")
+
+include("util/PatientAllocationResults.jl")
+include("util/NurseAllocationResults.jl")
+
+import .PatientAllocation: patient_allocation
+import .ReusableResourceAllocation: reusable_resource_allocation
+import .ForecastData: forecast
+import .BedsData: n_beds
+import .GeographicData: adjacencies
+import .NurseData: n_nurses
+import .PatientAllocationResults
+import .NurseAllocationResults
 
 export patient_allocation, reusable_resource_allocation
-export forecast, adjacencies, n_beds
+export forecast, adjacencies, n_beds, n_nurses
+export PatientAllocationResults, NurseAllocationResults
 
 end
