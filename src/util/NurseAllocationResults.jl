e8cf0ee5111428a63d36cd6e57d00f37c1adc743
Felix Parker <flixpar@gmail.com>
2020-06-07 22:09:15 -0400

    nurse alloc notebook update

--- src/util/NurseAllocationResults.jl
+++ src/util/NurseAllocationResults.jl
@@ -20,10 +20,12 @@ function results_all(
 	sent_matrix_table = results_sentmatrix_table(sent, locations)
 	sent_matrix_vis = results_sentmatrix_vis(sent, initial_nurses, locations)
 
-	avgload = sum(complete.load)/(N*T)
-
 	sent_to = Dict(locations[i] => locations[row] for (i,row) in enumerate(eachrow(sum(sent, dims=3)[:,:,1] .> 0)))
 	netsent = results_netsent(sent::Array{Float64,3}, start_date::Date)
+
+	avgload = sum(complete.load)/(N*T)
+	total_shortage = sum(complete.shortage)
+	total_sent = sum(complete.sent)
 
 	return (
 		summary_table=summary,
@@ -33,6 +35,8 @@ function results_all(
 		sent_to=sent_to,
 		netsent=netsent,
 		average_load=avgload,
+		total_shortage=total_shortage,
+		total_sent=total_sent,
 	)
 end
 
@@ -64,7 +68,7 @@ function results_summary(
 		total_nurse_days=[sum(current_nurses(i,t) for t in 1:T) for i in 1:N],
 		total_demand=total_demand,
 		total_shortage=shortage.(1:N),
-		# average_load=avgload.(1:N),
+		average_load=avgload.(1:N),
 	)
 
 	return summary
@@ -103,8 +107,8 @@ function results_complete(
 			shortage=[shortage(i,t) for t in 1:T],
 			load=[load(i,t) for t in 1:T],
 
-			# sent_to=[sum(sent[i,:,t])>0 ? collect(zip(locations[sent[i,:,t] .> 0], sent[i,sent[i,:,t].>0,t])) : "[]" for t in 1:T],
-			# sent_from=[sum(sent[:,i,t])>0 ? collect(zip(locations[sent[:,i,t] .> 0], sent[sent[:,i,t].>0,i,t])) : "[]" for t in 1:T],
+			sent_to=[sum(sent[i,:,t])>0 ? collect(zip(locations[sent[i,:,t] .> 0], sent[i,sent[i,:,t].>0,t])) : "[]" for t in 1:T],
+			sent_from=[sum(sent[:,i,t])>0 ? collect(zip(locations[sent[:,i,t] .> 0], sent[sent[:,i,t].>0,i,t])) : "[]" for t in 1:T],
 		)
 		outcomes = vcat(outcomes, single_state_outcome)
 	end
