2d4f2b8aca9633ab82e15f184da71b4d1acf38b8
Felix Parker <flixpar@gmail.com>
2020-10-21 09:41:24 -0400

    surge-level load balancing model

--- src/models/PatientAllocation.jl
+++ src/models/PatientAllocation.jl
@@ -373,6 +373,131 @@ function patient_surgelevel_redistribution(
 end
 
 ##############################################
+####### Surge-Level Load Balancing ###########
+##############################################
+
+function patient_surgelevel_loadbalance(
+		capacity::Array{<:Real},
+		initial_patients::Array{<:Real,1},
+		discharged_patients::Array{<:Real,2},
+		admitted_patients::Array{<:Real,2},
+		adj_matrix::BitArray{2}, los::Union{<:Distribution,Array{<:Real,1},Int};
+
+		capacity_cushion::Real=-1, capacity_weights::Array{<:Real,1}=Int[],
+		no_artificial_overflow::Bool=false, no_worse_overflow::Bool=false,
+		sent_penalty::Real=0, smoothness_penalty::Real=0,
+
+		sendreceive_gap::Int=0, min_send_amt::Real=0,
+		setup_cost::Real=0,
+
+		mipgap::Real=0.1, timelimit::Real=240, M::Real=1e6,
+		verbose::Bool=false,
+	)
+
+	###############
+	#### Setup ####
+	###############
+
+	N, T = size(admitted_patients)
+	C = size(capacity, 2)
+	check_sizes(initial_patients, discharged_patients, admitted_patients, capacity)
+
+	if 0 < capacity_cushion < 1
+		capacity = capacity .* (1.0 - capacity_cushion)
+	end
+
+	if isempty(capacity_weights)
+		capacity_weights = ones(Int, C)
+	end
+
+	L = discretize_los(los, T)
+
+	###############
+	#### Model ####
+	###############
+
+	model = Model(Gurobi.Optimizer)
+	if !verbose set_silent(model) end
+
+	set_optimizer_attribute(model, "MIPGap", mipgap)
+	set_optimizer_attribute(model, "TimeLimit", timelimit)
+
+	###############
+	## Variables ##
+	###############
+
+	@variable(model, sent[1:N,1:N,1:T] >= 0)
+	@variable(model, surgelevel[1:N,1:T,1:C], Bin)
+	@variable(model, load_obj[1:N,1:T] >= 0)
+
+	#################
+	## Expressions ##
+	#################
+
+	# expressions for the number of active patients
+	@expression(model, active_patients[i=1:N,t=1:T],
+		initial_patients[i]
+		- sum(discharged_patients[i,1:t])
+		+ sum(L[t-t₁+1] * (
+			admitted_patients[i,t₁]
+			- sum(sent[i,:,t₁])
+			+ sum(sent[:,i,t₁])
+		) for t₁ in 1:t)
+		+ sum(sent[i,:,t])
+	)
+	active_null = compute_active_null(initial_patients, discharged_patients, admitted_patients, L)
+
+	# expression for the patient overflow
+	@expression(model, overflow[i=1:N,t=1:T,c=1:C], active_patients[i,t] - capacity[i,c])
+
+	# objective function
+	objective = @expression(model, dot(capacity_weights, sum(load_obj, dims=(1,2))))
+
+	######################
+	## Hard Constraints ##
+	######################
+
+	# ensure the number of active patients is non-negative
+	@constraint(model, [i=1:N,t=1:T], active_patients[i,t] >= 0)
+
+	# only send new patients
+	@constraint(model, [t=1:T], sum(sent[:,:,t], dims=2) .<= admitted_patients[:,t])
+
+	# set surge-level
+	for i in 1:N, t in 1:T, c in 1:C
+		@constraint(model, M * surgelevel[i,t,c] >= overflow[i,t,c])
+		@constraint(model, M * (1 - surgelevel[i,t,c]) >= -overflow[i,t,c])
+	end
+
+	# load balancing objective
+	@constraint(model, [i=1:N,t=1:T],  (sum(surgelevel[i,t,:]) - mean(sum(surgelevel[:,t,:], dims=2))) <= load_obj[i,t])
+	@constraint(model, [i=1:N,t=1:T], -(sum(surgelevel[i,t,:]) - mean(sum(surgelevel[:,t,:], dims=2))) <= load_obj[i,t])
+
+	################################
+	## Optional Constraints/Costs ##
+	################################
+
+	enforce_adj!(model, sent, adj_matrix)
+	enforce_no_artificial_overflow!(model, no_artificial_overflow, active_patients, active_null, capacity)
+	enforce_no_worse_overflow!(model, no_worse_overflow, active_patients, active_null, capacity)
+	enforce_minsendamt!(model, sent, min_send_amt)
+	enforce_sendreceivegap!(model, sent, sendreceive_gap)
+
+	add_sent_penalty!(model, sent, objective, sent_penalty)
+	add_smoothness_penalty!(model, sent, objective, smoothness_penalty)
+	add_setup_cost!(model, sent, objective, setup_cost)
+
+	###############
+	#### Solve ####
+	###############
+
+	@objective(model, Min, objective)
+	optimize!(model)
+
+	return model
+end
+
+##############################################
 ############### Block Model ##################
 ##############################################
 
